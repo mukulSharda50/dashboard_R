b4a949a89d93cf4c24236c097c1be9a0ce9fe420
Mukul Sharda <mukulsharda5@gmail.com>
2022-11-09 21:48:17 +0530

    Implemented transaction tab item

--- Pages/home.R
+++ Pages/home.R
@@ -1,18 +1,18 @@
 library(shiny)
 library(shinydashboard)
 
-path = "/home/leaveit/Documents/Data_Sci_Dashboard/Pages"
+path = "/home/leaveit/Documents/Dashboard_data_sci/Pages"
 setwd(path)
-source("customer.R")
-path = "/home/leaveit/Documents/Data_Sci_Dashboard/Pages"
+source("customers.R")
+path = "/home/leaveit/Documents/Dashboard_data_sci/Pages"
 setwd(path)
-source("orders_func.R")
-path = "/home/leaveit/Documents/Data_Sci_Dashboard/Pages"
+source("orders.R")
+path = "/home/leaveit/Documents/Dashboard_data_sci/Pages"
 setwd(path)
 source("shipping.R")
-
-
-
+path = "/home/leaveit/Documents/Dashboard_data_sci/Pages"
+setwd(path)
+source("transactions.R")
 
 
 sidebar <- dashboardSidebar(
@@ -21,33 +21,38 @@ sidebar <- dashboardSidebar(
     menuItem("Customers", tabName="customers"),
     menuItem("Products", tabName = "products"),
     menuItem("Orders", tabName = "orders"),
-    menuItem("Shipping", tabName = "shipping")
+    menuItem("Shipping", tabName = "shipping"),
+    menuItem("Transaction", tabName = "transaction")
   )
 )
 
 body <- dashboardBody(
   tabItems(
+    tabItem("transaction", 
+      box(plotOutput("profit.dist")),
+      box(plotOutput("profit.month"))
+    ),
+    tabItem("shipping", 
+      box(plotOutput("ship.cost.dist"))
+    ),
+    tabItem("orders",
+      box(plotOutput("order.count"))      
+    ),
+    tabItem("products", 
+      h1("Products"),
+      h2("products")
+    ),
     tabItem("customers",
       h1("Customers"),
       fluidRow(width= 6, box(plotOutput("total.proportion.gender"))),
       fluidRow(
         box(selectInput("country", "Countries:", countries)),
         box(plotOutput("country.proportion.gender")),
-        ),
+      ),
       fluidRow(box(plotOutput("num.customers")))
-      ),
-    tabItem("products", 
-        h1("Products"),
-        h2("products")
-      ),
-    tabItem("orders",
-        box(plotOutput("order.count"))      
-      ),
-    tabItem("shipping", 
-      box(plotOutput("ship.cost.dist"))
-      )
+    )
   )
-)
+ )
 
 
 ui <- dashboardPage(
@@ -62,6 +67,8 @@ server <- function(input, output){
   output$num.customers = renderPlot(num.customers())
   output$order.count = renderPlot(order.Months())
   output$ship.cost.dist = renderPlot(ship.cost.dist())
+  output$profit.dist = renderPlot(profit.dist())
+  output$profit.month = renderPlot(profit.month())
 }
   
 
